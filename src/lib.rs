6947e8c1820eb73f2c4c53c1f3d4d5cf65103912
Jack <jack@example.com>
2024-12-30 17:42:08 -0700

    added split to printwrap which does, more or less, the samething print_wrap does, but produces a vector of strings instead of printing to stdout. split does not accomodate subsequent_tab padding.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
 use terminal_size::{Width, Height, terminal_size};
 
+/*
+	print_wrap
+		margins          minimum left and right margins
+		subsequent_tab   minimum additional left margin on lines after the first
+		line             the line to format
+
+	Prints <line> to standard output with minimum padding on 
+	both left and right sides. If the line is longer than the
+	terminal window (adding the margins), the line will be 
+	split on spaces and overflow will be printed on additional
+	lines as needed. The subsequent lines will have additional
+	padding on the left if subsequent_tab is greater than zero.
+*/
 pub fn print_wrap(margins:usize, subsequent_tab:usize, line:&str)
 {
 	let x;
@@ -60,4 +73,67 @@ pub fn print_wrap(margins:usize, subsequent_tab:usize, line:&str)
 		}
 
 	}
+}
+
+
+/*
+	split
+		width          maximum line width
+		line           the line to split
+
+	Splits <line> on spaces into one or more Strings where the
+	maximum length of each string is no more than <width>
+	characters wide. Weird things happen if <line> or part of
+	<line> has no spaces and is longer than width.
+*/
+pub fn split(width:usize, line:&str) -> Vec<String>
+{
+	let mut strings:Vec<String> = Vec::new();
+
+	let mut current_tab = 0;
+	if line.len() <= width
+	{
+		strings.push(String::from(line));
+	}
+	else
+	{
+		let bytes = line.as_bytes();
+		let mut index= width - current_tab - 1;
+		let mut start= 0 as usize;
+		while (index > start) && (index < bytes.len())
+		{
+			if bytes[index] == b' '
+			{
+				let slice = &line[start..index];
+				strings.push(String::from(slice));
+
+				current_tab = 0;
+				start = index + 1; // skip the space we're currently on
+				let pre_index = start + (width - current_tab) -1 ;
+				index = pre_index;
+				if index > bytes.len()
+				{
+					index = bytes.len() -1;
+				}
+				if (index-start) < (width - current_tab -1)
+				{
+					// end of the line. not enough left to split, so just print it and quit
+					let slice = &line[start..bytes.len()];
+					strings.push(String::from(slice));
+					break;
+				}
+			}
+			else
+			{
+				index = index - 1;
+			}
+		}
+		if index == start
+		{
+			// no spaces before width, so just print the first width characters
+			let slice = &line[start..(bytes.len())];
+			strings.push(String::from(slice));
+		}
+	}
+	return strings;
 }
